63f8940d46e900857835b0ab29f35f4a1411693a
Abraham <nencho_1@hotmail.com>
2021-01-19 22:35:33 +0100

    Revert api.json name

--- app_restaurant-main/Restaurant App/assets/JS/myFunction.js
+++ app_restaurant-main/Restaurant App/assets/JS/myFunction.js
@@ -10,7 +10,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 const fetchData = async () => {
     try{
-        const res = await fetch(`https://jsonplaceholder.typicode.com/todos`)
+        const res = await fetch('api.json')
         const data = await res.json();
         console.log(data)
     } catch (error) {
