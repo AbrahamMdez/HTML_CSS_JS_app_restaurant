47eaf1dcb4e5d111897acb9c602089de5155fa61
AbrahamMdez <nencho_1@hotmail.com>
2021-01-20 15:29:08 +0100

    Repaired rute api.json

--- app_restaurant-main/Restaurant App/assets/JS/myFunction.js
+++ app_restaurant-main/Restaurant App/assets/JS/myFunction.js
@@ -10,7 +10,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 const fetchData = async () => {
     try{
-        const res = await fetch('api.json')
+        const res = await fetch('../assets/JS/api.json');
         const data = await res.json();
         console.log(data)
     } catch (error) {
