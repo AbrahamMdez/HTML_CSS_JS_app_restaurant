4d870d4f342775d938352e74dcd51cef59e6147e
AbrahamMdez <nencho_1@hotmail.com>
2021-01-20 21:52:40 +0100

    Changed position to the id

--- app_restaurant-main/Restaurant App/assets/JS/myFunction.js
+++ app_restaurant-main/Restaurant App/assets/JS/myFunction.js
@@ -1,8 +1,6 @@
 'use strict'
 
-const items = document.querySelector('#items');
-const templateCard = document.querySelector('#template-card').content;
-const fragment = document.createDocumentFragment();
+const items = document.getElementById('items');
 
 document.addEventListener('DOMContentLoaded', () => {
     fetchData();
@@ -12,19 +10,23 @@ const fetchData = async () => {
     try{
         const res = await fetch('../assets/JS/api.json');
         const data = await res.json();
-        console.log(data)
+        pintData(data)
     } catch (error) {
         console.log(error)
     }
-};
+}
 
-const printCard = data => {
-    data.forEach(element => {
-        templateCard.querySelector('h3').textContent = element.name;
+const pintData = data => {
+    const templateCard = document.getElementById('template-card');
+    const fragment = document.createDocumentFragment();
+    console.log(templateCard)
+    data.forEach ( product => {
+        console.log(product)
+        templateCard.querySelector('h3').textContent = product.name;
 
         const clone = templateCard.cloneNode(true);
-        fragment.appendChild(clone)
+        fragment.appendChild(clone);
     })
 
-    items.appendChild(fragment);
+    items.appendChild(fragment)
 };
